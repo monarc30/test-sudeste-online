320d0b21d7af0ff27b6fd692cdfd5beeba11f610
Fernando Monarca <fernando.monarca@hotmail.com>
2020-07-21 19:19:03 -0300

    criando as migrations

---
+++ database/migrations/2020_07_21_215038_create_dosagens_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosagens', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->unsignedInteger('idproduto');
+            $table->unsignedInteger('idcultura');
+            $table->unsignedInteger('idpraga');
+
+            $table->string('dosagem', 200);
+            $table->timestamps();
+
+            $table->foreign('idproduto')->references('id')->on('produtos')->onDelete('CASCADE');
+            $table->foreign('idcultura')->references('id')->on('culturas')->onDelete('CASCADE');
+            $table->foreign('idpraga')->references('id')->on('pragas')->onDelete('CASCADE');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosagens');
+    }
+}
